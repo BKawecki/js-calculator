3ded19a78d2fd91a60fd13f6eb8b9c72a6755d94
Bartek Kawecki <bkawecki21@gmail.com>
2023-01-31 13:55:58 +0100

    added "add" function

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('This worksss');
+let currentResult = 0;
+currentResult += 10;
+
+function add() {
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
+
+
